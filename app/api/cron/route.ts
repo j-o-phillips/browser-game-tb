6469c4195fe37d6151613a878027947def850989
j-o-phillips <jphillips105@googlemail.com>
2024-04-02 17:25:52 +0200

    cron

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,5 +1,18 @@
+import { db } from "@/lib/db";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json({ success: true });
+  try {
+    const user = await db.user.update({
+      where: {
+        id: "6602c44cd192096d0ddcf491",
+      },
+      data: {
+        credits: 200,
+      },
+    });
+    return NextResponse.json({ success: true });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message });
+  }
 }
