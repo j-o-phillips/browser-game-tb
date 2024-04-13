47a22fd575abbce2f89a963d9005bf53460b94cd
j-o-phillips <jphillips105@googlemail.com>
2024-04-13 11:05:57 +0200

    3js window

--- app/game/bridge/page.tsx
+++ app/game/bridge/page.tsx
@@ -5,9 +5,26 @@ import BridgeNav from "@/components/bridge/BridgeNav";
 import ShipDetails from "@/components/bridge/ShipDetails";
 import InOrbit from "@/components/bridge/windowScenes/InOrbit";
 import { useUserContext } from "@/context/UserContext";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+// const InOrbitNoSSR = dynamic(
+//   () => import("@/components/bridge/windowScenes/InOrbit"),
+//   {
+//     ssr: false,
+//   }
+// );
 
 const Bridge = () => {
   const { userData } = useUserContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!userData) {
+      router.push("/game");
+    }
+  });
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[1fr_2fr_1fr] lg:grid-rows-3  lg:h-screen p-4 gap-4 text-primary-foreground">
@@ -21,7 +38,8 @@ const Bridge = () => {
         <h2>Target Image</h2>
       </div>
       <div className="">
-        <InOrbit />
+        {/* <InOrbitNoSSR /> */}
+        {userData && !userData.isLanded && <InOrbit />}
       </div>
       <div className="row-span-2 h-full  w-full bg-[#00f2ff52] border-2 border-cyan-500">
         <h1>Scanner Targets</h1>
--- components/bridge/windowScenes/InOrbit.tsx
+++ components/bridge/windowScenes/InOrbit.tsx
@@ -1,35 +1,29 @@
 "use client";
 
 import { Canvas, useLoader } from "@react-three/fiber";
-import CelestialBody from "../../map/system/celestialBody";
-import { useUserContext } from "@/context/UserContext";
-import { useParams, useRouter } from "next/navigation";
-import { TextureLoader } from "three/src/loaders/TextureLoader.js";
-import { useEffect, useState } from "react";
+
 import { Stars } from "@react-three/drei";
+import { TextureLoader } from "three";
+import CelestialBody from "@/components/map/system/celestialBody";
+import { useEffect } from "react";
+import { useUserContext } from "@/context/UserContext";
+import { useRouter } from "next/navigation";
 
 const InOrbit = () => {
-  // const { userData } = useUserContext();
-  // const router = useRouter();
+  const { userData } = useUserContext();
+  const router = useRouter();
   // let textureString = `/planetText/Arubula.jpg`;
 
-  // useEffect(() => {
-  //   if (!userData) {
-  //     router.push("/game");
-  //   }
-  // }, [userData, router]);
+  useEffect(() => {
+    if (!userData) {
+      router.push("/game");
+    }
+  }, [userData, router]);
 
-  // switch (userData?.currentLoc) {
-  //   case "Arubula":
-  //     textureString = `/planetText/Arubula.jpg`;
-  //     break;
-
-  //   default:
-  //     break;
-  // }
-
-  // const texture = useLoader(TextureLoader, "/planetText/Arubula.jpg");
-  const planet1Map = useLoader(TextureLoader, "/planetText/Arubula.jpg");
+  const texture = useLoader(
+    TextureLoader,
+    `/planetText/${userData?.currentLoc}.jpg`
+  );
 
   return (
     <div className=" bg-black h-full ">
@@ -44,16 +38,16 @@ const InOrbit = () => {
           saturation={0}
           fade
         />
-        {/* <CelestialBody
+        <CelestialBody
           name="Arubula"
-          texture={planet1Map}
+          texture={texture}
           positionX={-1}
           positionZ={0}
           scale={1}
           rotationSpeed={0.02}
           setObjectData={() => {}}
           unclickable
-        /> */}
+        />
       </Canvas>
     </div>
   );
